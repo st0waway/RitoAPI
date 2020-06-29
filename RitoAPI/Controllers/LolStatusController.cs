2e280f25b0ad1445ac518ef4b06034fabdd6adde
st0waway <lumixbd@gmail.com>
2020-06-29 15:46:29 +0300

    Move GetLeagueStatus from repo to controller

--- RitoAPI/Controllers/LolStatusController.cs
+++ RitoAPI/Controllers/LolStatusController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using RitoAPI.Models;
 using RitoAPI.Repositories;
+using System.IO;
+using System.Net;
 
 namespace RitoAPI.Controllers
 {
@@ -9,23 +13,52 @@ namespace RitoAPI.Controllers
     public class LolStatusController : ControllerBase
     {
         private readonly LolStatusRepo _repository;
+        private readonly string _apiKey;
 
-        public LolStatusController(LolStatusRepo lolStatusRepo)
+        public LolStatusController(IOptions<UserConfig> userConfigAccessor, LolStatusRepo lolStatusRepo)
         {
+            _apiKey = userConfigAccessor.Value.APIKey;
             _repository = lolStatusRepo;
         }
 
         [HttpGet]
         public ActionResult<ShardStatus> GetLeagueStatus(string server = "EUW1")
         {
-            var players = _repository.GetLeagueStatus(server);
-            if (players != null)
+            var url = "https://" + server + ".api.riotgames.com/lol/status/v3/shard-data" + "?api_key=" + _apiKey;
+            try
             {
-                return Ok(players);
+                var webRequest = WebRequest.Create(url) as HttpWebRequest;
+                webRequest.ContentType = "application/json";
+                webRequest.UserAgent = "Nothing";
+                using (var s = webRequest.GetResponse().GetResponseStream())
+                {
+                    using (var sr = new StreamReader(s))
+                    {
+                        var shardStatusJson = sr.ReadToEnd();
+                        var shardStatus = JsonConvert.DeserializeObject<ShardStatus>(shardStatusJson);
+                        return shardStatus;
+                    }
+                }
             }
-            else
+            catch (WebException e)
             {
-                return NotFound();
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var response = e.Response as HttpWebResponse;
+                    if (response != null)
+                    {
+                        var code = (int)response.StatusCode;
+                        return StatusCode(code);
+                    }
+                    else
+                    {
+                        return StatusCode(500);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
             }
         }
     }
--- RitoAPI/Repositories/LolStatusRepo.cs
+++ RitoAPI/Repositories/LolStatusRepo.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using RitoAPI.Models;
-using System.IO;
-using System.Net;
 
 namespace RitoAPI.Repositories
 {
@@ -13,22 +10,6 @@ namespace RitoAPI.Repositories
         public LolStatusRepo(IOptions<UserConfig> userConfigAccessor)
         {
             _apiKey = userConfigAccessor.Value.APIKey;
-        }
-        public ShardStatus GetLeagueStatus(string region)
-        {
-            var url = "https://" + region + ".api.riotgames.com/lol/status/v3/shard-data" + "?api_key=" + _apiKey;
-            var webRequest = WebRequest.Create(url) as HttpWebRequest;
-            webRequest.ContentType = "application/json";
-            webRequest.UserAgent = "Nothing";
-            using (var s = webRequest.GetResponse().GetResponseStream())
-            {
-                using (var sr = new StreamReader(s))
-                {
-                    var shardStatusJson = sr.ReadToEnd();
-                    var shardStatus = JsonConvert.DeserializeObject<ShardStatus>(shardStatusJson);
-                    return shardStatus;
-                }
-            }
         }
     }
 }
