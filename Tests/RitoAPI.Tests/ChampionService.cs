1d27d67739673a98e21e06ba5cb5b2649a59759e
st0waway <lumixbd@gmail.com>
2020-07-13 17:04:06 +0300

    Add GetFreeChampionInfo test

---
+++ Tests/RitoAPI.Tests/ChampionService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using RitoAPI.Models;
+using RitoAPI.Services;
+using Xunit;
+
+namespace RitoAPI.Tests
+{
+	public class ChampionServiceShould : IClassFixture<WebApplicationFactory<Startup>>
+	{
+		private readonly WebApplicationFactory<Startup> _factory;
+
+		public ChampionServiceShould(WebApplicationFactory<Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Fact]
+		public void GetFreeChampionInfo()
+		{
+			var service = (ChampionService)_factory.Services.GetService(typeof(ChampionService));
+			var freeChampionRotation = service.GetFreeChampionInfo();
+			Assert.IsType<ChampionInfo>(freeChampionRotation);
+			Assert.Equal(15, freeChampionRotation.freeChampionIds.Count);
+			Assert.Equal(10, freeChampionRotation.freeChampionIdsForNewPlayers.Count);
+			Assert.Equal(10, freeChampionRotation.maxNewPlayerLevel);
+		}
+	}
+}
