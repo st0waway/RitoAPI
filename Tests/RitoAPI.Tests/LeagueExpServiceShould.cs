29491b36ef3360d0b80f8e55cbed7f41b8fd9b15
st0waway <lumixbd@gmail.com>
2020-07-14 15:51:20 +0300

    Add GetLeagueExp test

---
+++ Tests/RitoAPI.Tests/LeagueExpServiceShould.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RitoAPI.Models;
+using RitoAPI.Services;
+using Xunit;
+
+namespace RitoAPI.Tests
+{
+	public class LeagueExpServiceShould : IClassFixture<WebApplicationFactory<Startup>>
+	{
+		private readonly WebApplicationFactory<Startup> _factory;
+
+		public LeagueExpServiceShould(WebApplicationFactory<Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Fact]
+		public void GetLeagueExp()
+		{
+			var service = (LeagueExpService)_factory.Services.GetService(typeof(LeagueExpService));
+			var leagueQueueEntry = service.GetLeagueExp("RANKED_SOLO_5x5", "PLATINUM", "IV");
+			Assert.IsType<List<LeagueEntryDTO>>(leagueQueueEntry);
+			Assert.Equal("PLATINUM", leagueQueueEntry[0].tier);
+			Assert.Equal("PLATINUM", leagueQueueEntry[5].tier);
+			Assert.Equal("PLATINUM", leagueQueueEntry[12].tier);
+		}
+	}
+}
