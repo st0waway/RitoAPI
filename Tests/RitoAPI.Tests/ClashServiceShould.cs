2609ad658bab205622975355f71b4f02b5e5fe1c
st0waway <lumixbd@gmail.com>
2020-07-16 14:49:26 +0300

    Add GetActiveClashPlayers template

---
+++ Tests/RitoAPI.Tests/ClashServiceShould.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RitoAPI.Models;
+using RitoAPI.Services;
+using Xunit;
+
+namespace RitoAPI.Tests
+{
+	public class ClashServiceShould : IClassFixture<WebApplicationFactory<Startup>>
+	{
+		private readonly WebApplicationFactory<Startup> _factory;
+
+		public ClashServiceShould(WebApplicationFactory<Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Fact]
+		public void GetActiveClashPlayers()
+		{
+			var service = (ClashService)_factory.Services.GetService(typeof(ClashService));
+			var summoner = service.GetActiveClashPlayers("");
+			Assert.IsType<List<ClashPlayerDto>>(summoner);
+		}
+	}
+}
