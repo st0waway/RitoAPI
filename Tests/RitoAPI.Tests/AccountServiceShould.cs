4c8882d57dbfc102816c3578031cafd08b286718
st0waway <lumixbd@gmail.com>
2020-07-16 16:38:11 +0300

    Add GetActiveShardForPlayer test

--- Tests/RitoAPI.Tests/AccountServiceShould.cs
+++ Tests/RitoAPI.Tests/AccountServiceShould.cs
@@ -22,5 +22,25 @@ namespace RitoAPI.Tests
 			Assert.Equal("6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA", account.puuid);
 			Assert.Equal("EUW", account.tagLine);
 		}
+
+		[Fact]
+		public void GetAccountByRiotId()
+		{
+			var service = (AccountService)_factory.Services.GetService(typeof(AccountService));
+			var account = service.GetAccountByRiotId("europe", "Lum1x", "EUW");
+			Assert.Equal("Lum1x", account.gameName);
+			Assert.Equal("6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA", account.puuid);
+			Assert.Equal("EUW", account.tagLine);
+		}
+
+		[Fact]
+		public void GetActiveShardForPlayer()
+		{
+			var service = (AccountService)_factory.Services.GetService(typeof(AccountService));
+			var activeShardInfo = service.GetActiveShardForPlayer("europe", "val", "6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA");
+			Assert.Equal("eu", activeShardInfo.activeShard);
+			Assert.Equal("6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA", activeShardInfo.puuid);
+			Assert.Equal("val", activeShardInfo.game);
+		}
 	}
 }
