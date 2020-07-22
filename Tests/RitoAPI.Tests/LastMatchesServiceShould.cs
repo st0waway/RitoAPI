8894b5f9eadbaeb98be930056de5bbdbdeb42913
st0waway <lumixbd@gmail.com>
2020-07-22 17:38:41 +0300

    Add LastMatchesService tests

---
+++ Tests/RitoAPI.Tests/LastMatchesServiceShould.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using RitoAPI.Services;
+
+using Xunit;
+
+namespace RitoAPI.Tests
+{
+	public class LastMatchesServiceShould : IClassFixture<WebApplicationFactory<Startup>>
+	{
+		private readonly WebApplicationFactory<Startup> _factory;
+
+		public LastMatchesServiceShould(WebApplicationFactory<Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Fact]
+		public void GetLastMatchesBySummoner()
+		{
+			var service = (LastMatchesService)_factory.Services.GetService(typeof(LastMatchesService));
+			var lastMatchesInfo = service.GetLastMatchesBySummoner("euw1", "Lum1x");
+			Assert.Equal("Lum1x", lastMatchesInfo.summonerName);
+		}
+
+		[Fact]
+		public void GetMatchesForMultipleSummoners()
+		{
+			var service = (LastMatchesService)_factory.Services.GetService(typeof(LastMatchesService));
+			var summonerNames = new List<string> {"Lum1x", "Dimodead"};
+			var lastMatchesInfo = service.GetMatchesForMultipleSummoners("euw1", summonerNames);
+			Assert.Equal("Lum1x", lastMatchesInfo[0].summonerName);
+			var index = 0;
+			foreach (var summonerName in summonerNames)
+			{
+				Assert.Equal(summonerName, lastMatchesInfo[index].summonerName);
+				index++;
+			}
+		}
+	}
+}
