0eff6886614099bb75bf4a1b6f275dbbd5459096
st0waway <lumixbd@gmail.com>
2020-07-22 16:52:40 +0300

    Remove unused usings

--- RitoAPI/Program.cs
+++ RitoAPI/Program.cs
@@ -1,9 +1,9 @@
 using System;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Configuration;
+
 using Serilog;
 
 namespace RitoAPI
