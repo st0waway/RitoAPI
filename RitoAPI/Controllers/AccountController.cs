8e613f93353016911da1ec1e08e68270e0a89f98
st0waway <lumixbd@gmail.com>
2020-07-22 14:53:29 +0300

    Add logs to AccountController

--- RitoAPI/Controllers/AccountController.cs
+++ RitoAPI/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 using RitoAPI.Services;
 
 namespace RitoAPI.Controllers
@@ -8,15 +10,18 @@ namespace RitoAPI.Controllers
 	public class AccountController : ControllerBase
 	{
 		private AccountService _accountService;
+		private readonly ILogger<SummonerController> _logger;
 
-		public AccountController(AccountService accountService)
+		public AccountController(AccountService accountService, ILogger<SummonerController> logger)
 		{
 			_accountService = accountService;
+			_logger = logger;
 		}
 
 		[HttpGet("puuid/{puuid}")]
 		public IActionResult GetAccountByPuuid(string region = "europe", string puuid = "6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA")
 		{
+			_logger.LogInformation("GetAccountByPuuid, region = {region}, puuid = {puuid}", region, puuid);
 			var summoner = _accountService.GetAccountByPuuid(region, puuid);
 
 			if (summoner == null)
@@ -30,6 +35,7 @@ namespace RitoAPI.Controllers
 		[HttpGet("riotid/{gameName}/{tagLine}")]
 		public IActionResult GetAccountByRiotId(string region = "europe", string gameName = "Lum1x", string tagLine = "EUW")
 		{
+			_logger.LogInformation("GetAccountByRiotId, region = {region}, gameName = {gameName}, tagLine = {tagLine}", region, gameName, tagLine);
 			var summoner = _accountService.GetAccountByRiotId(region, gameName, tagLine);
 
 			if (summoner == null)
@@ -43,6 +49,7 @@ namespace RitoAPI.Controllers
 		[HttpGet("active-shards/by-game/{game}/by-puuid/{puuid}")]
 		public IActionResult GetActiveShardForPlayer(string region = "europe", string game = "val", string puuid = "6N57LsQo6kOBNuz8yK8_lJ0KJmEzSH5cF2OhOfmwpQIOza2sZPb_vMb75A0wwdYSONBX26iNMburSA")
 		{
+			_logger.LogInformation("GetActiveShardForPlayer, region = {region}, game = {game}, puuid = {puuid}", region, game, puuid);
 			var summoner = _accountService.GetActiveShardForPlayer(region, game, puuid);
 
 			if (summoner == null)
