76f3b1310c4bc86995045bbcb8c193fef18e31fb
st0waway <lumixbd@gmail.com>
2020-07-23 17:56:50 +0300

    Change log format

--- RitoAPI/Middleware/RequestResponseLoggingMiddleware.cs
+++ RitoAPI/Middleware/RequestResponseLoggingMiddleware.cs
@@ -31,12 +31,12 @@ namespace RitoAPI.Middleware
 			context.Request.EnableBuffering();
 			await using var requestStream = _recyclableMemoryStreamManager.GetStream();
 			await context.Request.Body.CopyToAsync(requestStream);
-			_logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
-								   $"Schema:{context.Request.Scheme} " +
-								   $"Host: {context.Request.Host} " +
-								   $"Path: {context.Request.Path} " +
-								   $"QueryString: {context.Request.QueryString} " +
-								   $"Request Body: {ReadStreamInChunks(requestStream)}");
+			_logger.LogInformation("Http Request Information:{newLine}" +
+								   "Schema:{schema} " +
+								   "Host: {host} " +
+								   "Path: {path} " +
+								   "QueryString: {queryString} " +
+								   "Request Body: {body}", Environment.NewLine, context.Request.Scheme, context.Request.Host, context.Request.Path, context.Request.QueryString, ReadStreamInChunks(requestStream));
 			context.Request.Body.Position = 0;
 		}
 		private static string ReadStreamInChunks(Stream stream)
@@ -65,12 +65,12 @@ namespace RitoAPI.Middleware
 			context.Response.Body.Seek(0, SeekOrigin.Begin);
 			var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
 			context.Response.Body.Seek(0, SeekOrigin.Begin);
-			_logger.LogInformation($"Http Response Information:{Environment.NewLine}" +
-								   $"Schema:{context.Request.Scheme} " +
-								   $"Host: {context.Request.Host} " +
-								   $"Path: {context.Request.Path} " +
-								   $"QueryString: {context.Request.QueryString} " +
-								   $"Response Body: {text}");
+			_logger.LogInformation("Http Response Information:{newLine}" +
+											   "Schema:{schema} " +
+											   "Host: {host} " +
+											   "Path: {path} " +
+											   "QueryString: {queryString} " +
+											   "Response Body: {body}", Environment.NewLine, context.Request.Scheme, context.Request.Host, context.Request.Path, context.Request.QueryString, text);
 			await responseBody.CopyToAsync(originalBodyStream);
 		}
 	}
