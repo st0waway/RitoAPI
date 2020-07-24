13812cf948ca7512416621838a58bb66ab44f942
st0waway <lumixbd@gmail.com>
2020-07-24 16:24:50 +0300

    Modify SummonerServiceTest

--- Tests/RitoAPI.Tests/SummonerServiceTest.cs
+++ Tests/RitoAPI.Tests/SummonerServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
-using RitoAPI.Models;
+
 using RitoAPI.Services;
+
 using Xunit;
 
 namespace RitoAPI.Tests
@@ -14,48 +15,64 @@ namespace RitoAPI.Tests
 			_factory = factory;
 		}
 
-		[Fact]
-		public void GetSummonerByName()
-		{
+		[Theory]
+		[InlineData("euw1", "RGE lnspired", "NGh2899uPTAjNcpigwqbR9jlOqaJovDEtUhE4GTjzb7DdKA")]
+		[InlineData("na1", "C9 Zven", "cMeedg7QLIaH2e71EpImpBmgiOcWVjzY6H7ubcQh9a2dVTI")]
+		[InlineData("ru", "Teufelslied", "vATAfvw3BMbMKky6iMHtghHgAovcphQ8xxHmvK8yj_H9jhKhEzlPYAug")]
+		[InlineData("oc1", "gunkrab", "xaU9a1WoFS6SX3IpwNyoMgjHitwYKxxg89BsQx8vdJQWvuI")]
+		[InlineData("jp1", "biewanad", "fnscZct1rXvfjLWPzta9VDAzeonQEWXct9N6eXQHbHV3YaIGIXvinyTx")]
+		public void GetSummonerByName_ShouldGetAccountId(string region, string name, string expected)
+		{				
 			var service = (SummonerService)_factory.Services.GetService(typeof(SummonerService));
-			var summoner = service.GetSummonerByName("euw1", "LumiX");
-			Assert.IsType<SummonerDTO>(summoner);			
-			Assert.Equal("KfujoSa2slNW8QP8ne6AkZb5PNtGK8aSxX80LPJQ6CyOMw", summoner.AccountId);
-			Assert.Equal("xgyV-Wuddie5sg_XjTSOPWMM64CVIegtl-8jRpLCQZnYkSg", summoner.Id);
-			Assert.Equal("uTkDriAmEwNbyev-RAdu8mppfJyrVhL393CvxYp6OMt0IyL4OzeYHVN9nbQMtpPhcIKmpJSfRjuDJg", summoner.Puuid);
+			var summoner = service.GetSummonerByName(region, name);
+			string actual = summoner.AccountId;
+
+			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void GetSummonerByAccount()
+		[Theory]
+		[InlineData("euw1", "NGh2899uPTAjNcpigwqbR9jlOqaJovDEtUhE4GTjzb7DdKA", "RGE lnspired")]
+		[InlineData("na1", "cMeedg7QLIaH2e71EpImpBmgiOcWVjzY6H7ubcQh9a2dVTI", "C9 Zven")]
+		[InlineData("ru", "vATAfvw3BMbMKky6iMHtghHgAovcphQ8xxHmvK8yj_H9jhKhEzlPYAug", "Teufelslied")]
+		[InlineData("oc1", "xaU9a1WoFS6SX3IpwNyoMgjHitwYKxxg89BsQx8vdJQWvuI", "gunkrab")]
+		[InlineData("jp1", "fnscZct1rXvfjLWPzta9VDAzeonQEWXct9N6eXQHbHV3YaIGIXvinyTx","biewanad")]
+		public void GetSummonerByAccount_ShouldGetSummonerName(string region, string accountId, string expected)
 		{
 			var service = (SummonerService)_factory.Services.GetService(typeof(SummonerService));
-			var summoner = service.GetSummonerByAccount("euw1", "KfujoSa2slNW8QP8ne6AkZb5PNtGK8aSxX80LPJQ6CyOMw");
-			Assert.IsType<SummonerDTO>(summoner);
-			Assert.Equal("LumiX", summoner.Name);
-			Assert.Equal("xgyV-Wuddie5sg_XjTSOPWMM64CVIegtl-8jRpLCQZnYkSg", summoner.Id);
-			Assert.Equal("uTkDriAmEwNbyev-RAdu8mppfJyrVhL393CvxYp6OMt0IyL4OzeYHVN9nbQMtpPhcIKmpJSfRjuDJg", summoner.Puuid);
+			var summoner = service.GetSummonerByAccount(region, accountId);
+			var actual = summoner.Name;
+			
+			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void GetSummonerByPUUID()
+		[Theory]
+		[InlineData("euw1", "9HtsonXM5dtY39cbySr3kHLRlTcqFkv3PTRAH5VjEOyLmLGLNLAvlRhM1pNu02OB9c0YnXDQHU7MLQ", "RGE lnspired")]
+		[InlineData("na1", "wM14gJ-e7QxYFSj7gUOuTGEwMz_Ec5P3hNx878j8L6sLa7lPSRAwGJVHXKARSVfhY4tH-UJeAveOsw", "C9 Zven")]
+		[InlineData("ru", "fKO_k0LRkgyeXvma1n7Ih7o5aWdirDn74-1XIGEgUdtSTASIOe7NrCG6IY1MYaqO6OJGRgUonTvYjw", "Teufelslied")]
+		[InlineData("oc1", "IUnVydyblwcuuwg_4KJ6iuWY48JR4m4EuU1ppcbXRU70gptArBiC-K4Fj4w0HW4no_lTowOVkqLpLg", "gunkrab")]
+		[InlineData("jp1", "7ndTViYQHGMMgTuKUGNXvkekrRP9k31MvjkW0nk6j7v2W0DLi5x7aKV5OTygUuKWH6kuubaJEnxdbg", "biewanad")]
+		public void GetSummonerByPUUID_ShouldGetSummonerName(string region, string puuid, string expected)
 		{
 			var service = (SummonerService)_factory.Services.GetService(typeof(SummonerService));
-			var summoner = service.GetSummonerByPUUID("euw1", "uTkDriAmEwNbyev-RAdu8mppfJyrVhL393CvxYp6OMt0IyL4OzeYHVN9nbQMtpPhcIKmpJSfRjuDJg");
-			Assert.IsType<SummonerDTO>(summoner);
-			Assert.Equal("LumiX", summoner.Name);
-			Assert.Equal("KfujoSa2slNW8QP8ne6AkZb5PNtGK8aSxX80LPJQ6CyOMw", summoner.AccountId);
-			Assert.Equal("xgyV-Wuddie5sg_XjTSOPWMM64CVIegtl-8jRpLCQZnYkSg", summoner.Id);
+			var summoner = service.GetSummonerByPUUID(region, puuid);
+			var actual = summoner.Name;
+						
+			Assert.Equal(expected, actual);			
 		}
 
-		[Fact]
-		public void GetSummonerBySummonerID()
+		[Theory]
+		[InlineData("euw1", "gMe0Nt4CpreMJRa9e-8u71tHtS_p-G2WpI5EjFCjjZXUAVk", "RGE lnspired")]
+		[InlineData("na1", "R84hfPnFbZbisUxqc75-e4dxzIS99ExnjqACgn7b5xzN1E0", "C9 Zven")]
+		[InlineData("ru", "N8kT7Dgjk2_AGCznbanIkSRtRF9U_g3oRCn3BHO-zsWBVwg", "Teufelslied")]
+		[InlineData("oc1", "ySQv_pKT-lhCNa-0Swb_Gm7-s7vkTpOCkPZ7QZubHJqq", "gunkrab")]
+		[InlineData("jp1", "v7KFUgdLNT49aR6vVyO2zOxbab_INOUwWkOpMJElPYTdPTY", "biewanad")]
+		public void GetSummonerBySummonerID_ShouldGetSummonerName(string region, string summonerId, string expected)
 		{
 			var service = (SummonerService)_factory.Services.GetService(typeof(SummonerService));
-			var summoner = service.GetSummonerBySummonerID("euw1", "xgyV-Wuddie5sg_XjTSOPWMM64CVIegtl-8jRpLCQZnYkSg");
-			Assert.IsType<SummonerDTO>(summoner);
-			Assert.Equal("LumiX", summoner.Name);
-			Assert.Equal("KfujoSa2slNW8QP8ne6AkZb5PNtGK8aSxX80LPJQ6CyOMw", summoner.AccountId);
-			Assert.Equal("uTkDriAmEwNbyev-RAdu8mppfJyrVhL393CvxYp6OMt0IyL4OzeYHVN9nbQMtpPhcIKmpJSfRjuDJg", summoner.Puuid);
+			var summoner = service.GetSummonerBySummonerID(region, summonerId);
+			var actual = summoner.Name;
+
+			Assert.Equal(expected, actual);		
 		}
 	}
 }
