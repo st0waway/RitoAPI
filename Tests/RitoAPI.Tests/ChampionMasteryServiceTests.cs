3b15b1b8223b3cdf60f01461fe7253687aa5ff44
st0waway <lumixbd@gmail.com>
2020-07-29 15:07:14 +0300

    Update GetChampionMasteryByIdandChampionId_ReturnsDataForCorrectChampion

--- Tests/RitoAPI.Tests/ChampionMasteryServiceTests.cs
+++ Tests/RitoAPI.Tests/ChampionMasteryServiceTests.cs
@@ -30,13 +30,21 @@ namespace RitoAPI.Tests
 			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void GetChampionMasteryByIdandChampionId()
+		[Theory]
+		[InlineData("euw1", "ohb-yL5WsfR7pAh0psgAspPTBh3MuN2vdNIMxNC02AE2QVk", 1)]
+		[InlineData("na1", "R84hfPnFbZbisUxqc75-e4dxzIS99ExnjqACgn7b5xzN1E0", 1)]
+		[InlineData("ru", "N8kT7Dgjk2_AGCznbanIkSRtRF9U_g3oRCn3BHO-zsWBVwg", 1)]
+		[InlineData("oc1", "ySQv_pKT-lhCNa-0Swb_Gm7-s7vkTpOCkPZ7QZubHJqq", 5)]
+		[InlineData("jp1", "v7KFUgdLNT49aR6vVyO2zOxbab_INOUwWkOpMJElPYTdPTY", 1)]
+		public void GetChampionMasteryByIdandChampionId_ReturnsDataForCorrectChampion(string region, string summonerId, int championId)
 		{
+			var expected = championId;
+
 			var service = (ChampionMasteryService)_factory.Services.GetService(typeof(ChampionMasteryService));
-			var championMasteries = service.GetChampionMasteryByIdandChampionId("euw1", "ohb-yL5WsfR7pAh0psgAspPTBh3MuN2vdNIMxNC02AE2QVk", 1);
-			Assert.Equal("ohb-yL5WsfR7pAh0psgAspPTBh3MuN2vdNIMxNC02AE2QVk", championMasteries.summonerId);
-			Assert.Equal(1, championMasteries.championId);
+			var championMasteries = service.GetChampionMasteryByIdandChampionId(region, summonerId, championId);
+			var actual = championMasteries.championId;
+			
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
