a608261eaf664e8764547151cf8851966ff95462
st0waway <lumixbd@gmail.com>
2020-07-29 15:14:12 +0300

    Update GetChampionMasteryScore_ReturnsMasteryScoreGreaterThanFive

--- Tests/RitoAPI.Tests/ChampionMasteryServiceTests.cs
+++ Tests/RitoAPI.Tests/ChampionMasteryServiceTests.cs
@@ -47,12 +47,18 @@ namespace RitoAPI.Tests
 			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void GetChampionMasteryScore()
+		[Theory]
+		[InlineData("euw1", "ohb-yL5WsfR7pAh0psgAspPTBh3MuN2vdNIMxNC02AE2QVk")]
+		[InlineData("na1", "R84hfPnFbZbisUxqc75-e4dxzIS99ExnjqACgn7b5xzN1E0")]
+		[InlineData("ru", "N8kT7Dgjk2_AGCznbanIkSRtRF9U_g3oRCn3BHO-zsWBVwg")]
+		[InlineData("oc1", "ySQv_pKT-lhCNa-0Swb_Gm7-s7vkTpOCkPZ7QZubHJqq")]
+		[InlineData("jp1", "v7KFUgdLNT49aR6vVyO2zOxbab_INOUwWkOpMJElPYTdPTY")]
+		public void GetChampionMasteryScore_ReturnsMasteryScoreGreaterThanFive(string region, string summonerId)
 		{
 			var service = (ChampionMasteryService)_factory.Services.GetService(typeof(ChampionMasteryService));
-			var championMasteryLevel = service.GetChampionMasteryScore("euw1", "ohb-yL5WsfR7pAh0psgAspPTBh3MuN2vdNIMxNC02AE2QVk");
-			Assert.IsType<int>(championMasteryLevel);
+			var championMasteryLevel = service.GetChampionMasteryScore(region, summonerId);
+			var actual = championMasteryLevel;
+			Assert.True(actual > 5);
 		}
 	}
 }
