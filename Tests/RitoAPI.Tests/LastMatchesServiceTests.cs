33ecaa29921b2ed414fe48544e783f374af1ae31
st0waway <lumixbd@gmail.com>
2020-07-30 16:07:46 +0300

    Add GetMatchesForMultipleSummoners_UsingClassData

--- Tests/RitoAPI.Tests/LastMatchesServiceTests.cs
+++ Tests/RitoAPI.Tests/LastMatchesServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc.Testing;
 
@@ -23,13 +24,13 @@ namespace RitoAPI.Tests
 		[InlineData("ru", "Yu meng")]
 		[InlineData("jp1", "biewanad")]
 		[InlineData("kr", "T1 BurdoI")]
-		public void GetLastMatchesBySummoner_ReturnsCorrectSummonerName(string region, string summonerName )
+		public void GetLastMatchesBySummoner_ReturnsCorrectSummonerName(string region, string summonerName)
 		{
 			var expected = summonerName;
 			var service = (LastMatchesService)_factory.Services.GetService(typeof(LastMatchesService));
 			var lastMatchesInfo = service.GetLastMatchesBySummoner(region, summonerName);
 			var actual = lastMatchesInfo.summonerName;
-			
+
 			Assert.Equal(expected, actual);
 		}
 
@@ -37,7 +38,7 @@ namespace RitoAPI.Tests
 		public void GetMatchesForMultipleSummoners()
 		{
 			var service = (LastMatchesService)_factory.Services.GetService(typeof(LastMatchesService));
-			var summonerNames = new List<string> {"Lum1x", "Dimodead"};
+			var summonerNames = new List<string> { "Lum1x", "Dimodead" };
 			var lastMatchesInfo = service.GetMatchesForMultipleSummoners("euw1", summonerNames);
 			Assert.Equal("Lum1x", lastMatchesInfo[0].summonerName);
 			var index = 0;
@@ -45,6 +46,39 @@ namespace RitoAPI.Tests
 			{
 				Assert.Equal(summonerName, lastMatchesInfo[index].summonerName);
 				index++;
+			}
+		}
+
+		[Theory, ClassData(typeof(SummonerNamesData))]
+		public void GetMatchesForMultipleSummoners_UsingClassData(string region, string summonerName1, string summonerName2)
+		{
+			var service = (LastMatchesService)_factory.Services.GetService(typeof(LastMatchesService));
+			var summonerNames = new List<string> { summonerName1, summonerName2 };
+			var lastMatchesInfo = service.GetMatchesForMultipleSummoners(region, summonerNames);
+			var index = 0;
+			foreach (var summonerName in summonerNames)
+			{
+				Assert.Equal(summonerName, lastMatchesInfo[index].summonerName);
+				index++;
+			}
+		}
+
+		public class SummonerNamesData : IEnumerable<object[]>
+		{
+			private readonly List<object[]> _summonerNames = new List<object[]>()
+			{
+				new object[] {"euw1", "Lum1x", "Dimodead" },
+				new object[] {"euw1", "Spb Taipan", "HydroPat" }
+			};
+
+			public IEnumerator<object[]> GetEnumerator()
+			{
+				return _summonerNames.GetEnumerator();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return GetEnumerator();
 			}
 		}
 	}
